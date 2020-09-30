805fa8011340c87c3bb48c48566a9879e3f1489a
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2020-09-30 15:25:15 -0400

    sanitize non-breaking space in property key

--- wikinfo.el
+++ wikinfo.el
@@ -148,6 +148,8 @@ It must return a single result. If nil, the user is prompted."
   "Return santizied th STRING."
   (thread-last
       (downcase string)
+    ;;@OPTIMIZATION: combine this with following regexp
+    (replace-regexp-in-string " " " ")
     (replace-regexp-in-string "\\(?:[[:space:]]\\)" "-")
     (replace-regexp-in-string "[^[:alnum:]-]" "")
     (replace-regexp-in-string "--" "-")
